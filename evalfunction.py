ed834a7b81b025024fcbf4e045af2aa2e68ada5c
norris03 <65301566+norris03@users.noreply.github.com>
2024-05-21 23:09:49 +0200

    Update evalfunction.py

--- evalfunction.py
+++ evalfunction.py
@@ -2,10 +2,19 @@ def evalfunction(game_state):
     if len(game_state) == 0:
         return -2**10
     else:
+        head = game_state["you"]["head"]
+        x = head["x"]
+        y = head["y"]
         max_x = game_state["board"]["width"]
         max_y = game_state["board"]["height"]
         mid_x = max_x/2
         mid_y = max_y/2
-        diff_x = abs(game_state["you"]["head"]["x"]-mid_x)
-        diff_y = abs(game_state["you"]["head"]["y"]-mid_y)
-        return game_state["you"]["health"]*10 - diff_x - diff_y
+        diff_x = abs(x-mid_x)
+        diff_y = abs(y-mid_y)
+        food = game_state["board"]["food"]
+        min_distance_to_food = max_x+max_y
+        for pof in food:
+            distance_to_food = abs(x-pof["x"])+abs(y-pof["y"])
+            if distance_to_food < min_distance_to_food:
+                min_distance_to_food = distance_to_food
+        return  10*game_state["you"]["health"] - min_distance_to_food -(diff_x + diff_y)
